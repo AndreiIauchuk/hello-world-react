c23b5c08a7397c7f051937600c3afa617d9feed1
AndreiIauchuk <iovchukandrew@gmail.com>
2024-04-21 20:09:34 +0200

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
 import App from './App';
 
-test('requested Golang message', () => {
-  render(App());
-  const linkElement = screen.getByText('Hello from Golang!');
-  expect(linkElement).toBeInTheDocument();
+describe('App component test', () => {
+  it('fetches message and renders it', async () => {
+    fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ text: 'Hello from Golang!' }),
+      })
+    );
+
+    render(<App />);
+    expect(fetch).toHaveBeenCalledWith('say-hello');
+    await waitFor(() => screen.getByText('Hello from Golang!'));
+    expect(screen.getByText('Hello from Golang!')).toBeInTheDocument();
+  });
+
+  it('handles fetch error', async () => {
+    const consoleErrorSpy = jest.spyOn(console, 'error');
+    const errorMessage = 'Fetch error'
+    fetch = jest.fn(() => Promise.reject(errorMessage))
+    render(<App />);
+    expect(fetch).toHaveBeenCalledWith('say-hello');
+    await waitFor(() =>
+      expect(consoleErrorSpy).toHaveBeenCalledWith('Error fetching message:', errorMessage)
+    );
+  });
 });
